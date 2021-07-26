b5cd3dba851535b963f6692926956df57f71cfe8
Ysp <espi8624@gmail.com>
2021-07-26 18:39:28 +0900

    Add Customer

--- server.js
+++ server.js
@@ -21,10 +21,9 @@ const connection = mysql.createConnection({
 connection.connect();
 
 app.get('/api/customers', (req,res) => {
-    connection.query(
-      "SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
         res.send(rows);
-      }  
+      }
     );
 });
 
