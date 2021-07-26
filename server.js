f5f4f4b115c684bab9546298e0a491eef912da41
Ysp <espi8624@gmail.com>
2021-07-26 18:56:18 +0900

    add database json file

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
+// database info
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
