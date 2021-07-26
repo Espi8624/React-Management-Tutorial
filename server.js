ffe861216b45f9a48b5736b11f35477da362bb9e
Ysp <espi8624@gmail.com>
2021-07-26 18:59:31 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
-// database info
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
